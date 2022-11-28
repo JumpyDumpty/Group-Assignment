2a1c02d5f3a7634ad5584f4ec36ef7dd1331dce7
Yan <yan.kosals@mail.utoronto.ca>
2022-11-27 19:15:40 -0500

    DEV-1.2 Filter and Highlight strategies

---
+++ Filter.java
@@ -0,0 +1,72 @@
+import Strategy.Strategy;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import treeviz.MunicipalTree;
+
+import java.util.List;
+
+/**
+ * A class implementing the strategy for filtering trees with a specific name from the map
+ */
+public class Filter implements Strategy {
+    //parameters and comments copied from TreeFilterEventHandler class!
+    private ChoiceBox treeSelect; //select box ref
+    private TextField txtSummary; //summary of tree counts
+    private TreeViewer treeView; //the tree view
+
+    /**
+     * Constructor
+     * @param view the tree view
+     */
+    public Filter(TreeViewer view){
+        this.treeView = view;
+        this.treeSelect = view.getTreeSelect();
+        this.txtSummary = view.getTxtSummary();
+
+    }
+    /**
+     * Copied from the handle() method within the TreeFilterEventHandler class!
+     * 1. Clear (or UNDO) all the circles from the existing view
+     * 2. Cycle thru the list of trees and redraw circles that correspond to the tree type selected
+     * 3. Remember to register any circles you draw on the "undo" stack, so they can be undone later!
+     * 4. Remember also that if the user selects "ALL TREES", all the trees should be drawn
+     */
+    @Override
+    public void execute() {
+        String selectedType = (String) treeSelect.getValue();
+        List<MunicipalTree> trees = treeView.getTrees();
+        while (!treeView.getUndoStack().isEmpty()){
+            Object circle = treeView.getUndoStack().pop();
+            treeView.getAnchorRoot().getChildren().remove(circle);
+        }
+        double[] boundries = treeView.getBoundaries();
+        double llx = boundries[0];
+        double lly = boundries[1];
+        double urx = boundries[2];
+        double ury = boundries[3];
+        int height = treeView.getHeight();
+        int width = treeView.getWidth();
+        double[] coords;
+        for (MunicipalTree t: trees){
+            if (selectedType == null || t.getName().equalsIgnoreCase(selectedType) ||
+                    selectedType.equalsIgnoreCase("ALL TREES")){
+                coords = t.getLoc().getCoords();
+                double yval = (double) height - height*((coords[1] - llx)/(urx - llx));
+                double xval = (double) width - width*((coords[0] - lly)/(ury - lly));
+                if (yval < height & yval > 0 & xval < width & xval > 0){
+                    Circle circle = new Circle();
+                    circle.setCenterX(xval);
+                    circle.setCenterY(yval);
+                    circle.setRadius(3);
+                    circle.setFill(Color.RED);
+                    treeView.getAnchorRoot().getChildren().add(circle); //attach each circle to the scene graph
+                    treeView.getUndoStack().add(circle);
+                }
+            }
+        }
+        this.txtSummary.setText("Trees selected: " + treeView.getUndoStack().size());
+    }
+
+}
---
+++ Highlight.java
@@ -0,0 +1,128 @@
+import Strategy.Strategy;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import treeviz.MunicipalTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implements highlighting trees with a specific name as a strategy
+ */
+public class Highlight implements Strategy {
+    private ChoiceBox treeSelect; //select box ref
+    private TextField txtSummary; //summary of tree counts
+    private TreeViewer treeView; //the tree view
+    private double llx, lly, urx, ury; //boundaries of the map
+    private int height, width; //Height and width of the tree view
+
+    /**
+     * Constructor
+     *
+     * @param view the tree view
+     */
+    public Highlight(TreeViewer view) {
+        this.treeView = view;
+        this.treeSelect = view.getTreeSelect();
+        this.txtSummary = view.getTxtSummary();
+
+        double[] boundries = treeView.getBoundaries();
+        this.llx = boundries[0];
+        this.lly = boundries[1];
+        this.urx = boundries[2];
+        this.ury = boundries[3];
+        this.height = treeView.getHeight();
+        this.width = treeView.getWidth();
+    }
+
+    /**
+     * Returns the coordinates of the tree on the map
+     * @param tree MunicipalTree object
+     * @return an array of two doubles representing the coordinates of the tree on the map
+     */
+    private double[] getTreeXYVals(MunicipalTree tree) {
+        double[] coords;
+        coords = tree.getLoc().getCoords();
+
+        double[] vals = new double[2];
+        vals[0] = (double) width - width * ((coords[0] - lly) / (ury - lly));
+        vals[1] = (double) height - height * ((coords[1] - llx) / (urx - llx));
+
+        return vals;
+    }
+
+
+    /**
+     * Helper method that redraws all the non-selected trees and also returns
+     * an ArrayList of selected trees
+     *
+     * @return ArrayList of selected trees
+     */
+    private ArrayList<MunicipalTree> drawNonSelected() {
+        String selectedType = (String) treeSelect.getValue();
+
+        List<MunicipalTree> trees = treeView.getTrees();
+        ArrayList<MunicipalTree> selectedTrees = new ArrayList<>();
+
+        while (!treeView.getUndoStack().isEmpty()) {
+            Object circle = treeView.getUndoStack().pop();
+            treeView.getAnchorRoot().getChildren().remove(circle);
+        }
+
+        for (MunicipalTree t : trees) {
+            if (t.getName().equalsIgnoreCase(selectedType)) {
+                selectedTrees.add(t);
+            } else {
+                double[] xYVals = getTreeXYVals(t);
+                double xval = xYVals[0];
+                double yval = xYVals[1];
+                if (yval < height & yval > 0 & xval < width & xval > 0) {
+                    Circle circle = new Circle();
+                    circle.setCenterX(xval);
+                    circle.setCenterY(yval);
+                    circle.setRadius(3);
+                    circle.setFill(Color.DARKGREEN);
+                    treeView.getAnchorRoot().getChildren().add(circle); //attach each circle to the scene graph
+                    treeView.getUndoStack().add(circle);
+                }
+            }
+        }
+        return selectedTrees;
+    }
+
+    /**
+     * Helper method that draws the selected trees as green
+     * @param selectedTrees selected trees to be highlighted
+     */
+    private void drawSelected(ArrayList<MunicipalTree> selectedTrees) {
+        int selectedNum = 0;
+        for (MunicipalTree t : selectedTrees) {
+            double[] xYVals = getTreeXYVals(t);
+            double xval = xYVals[0];
+            double yval = xYVals[1];
+            if (yval < height & yval > 0 & xval < width & xval > 0) {
+                Circle circle = new Circle();
+                circle.setCenterX(xval);
+                circle.setCenterY(yval);
+                circle.setRadius(4);
+                circle.setFill(Color.RED);
+                treeView.getAnchorRoot().getChildren().add(circle); //attach each circle to the scene graph
+                treeView.getUndoStack().add(circle);
+                selectedNum++;
+            }
+        }
+        this.txtSummary.setText("Trees highlighted: " + selectedNum);
+    }
+    /**
+     * 1. Clear the map of the trees using the undo stack
+     * 2. Redraw the non-selected trees as red circles and selected
+     * trees as green circles over top the previously drawn trees.
+     */
+    @Override
+    public void execute () {
+        ArrayList<MunicipalTree> selected = drawNonSelected();
+        drawSelected(selected);
+    }
+}
